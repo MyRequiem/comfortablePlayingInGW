132a9490ec195deb66dd9d95148b158cdb435282
MyRequiem <mailto:mrvladislavovich@gmail.com>
2015-07-31 09:35:03 +0300

    initial project version

---
+++ README.md
@@ -0,0 +1,5 @@
+![Ganjawars.ru](http://images.ganjawars.ru/img/apple144.png)
+
+#### Many buns for a comfortable game in [Ganjawars.ru](http://www.ganjawars.ru/index.php)
+(underconstruction)
+
---
+++ _comfortablePlayingInGW.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name            Comfortable Playing In GW
+// @namespace       using namespace std;
+// @description     Разноцветные плюшки для ganjawars.ru
+// @include         http://www.ganjawars.ru/*
+// @include         http://quest.ganjawars.ru*
+// @grant           none
+// @license         MIT
+// @version         1.0-310715
+// @author          MyRequiem
+// ==/UserScript==
+
+/*global unsafeWindow: true , ActiveXObject: true */
+/*jslint browser: true, todo: true, passfail: true */
+
+(function () {
+    'use strict';
+
+
+}());
+
