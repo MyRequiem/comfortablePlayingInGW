6c689192899b5048c2dd621a3f708058deaf15f7
MyRequiem <mailto:mrvladislavovich@gmail.com>
2015-08-03 03:06:30 +0300

    added settings && module 'NotGiveCannabisLeaf'

---
+++ comfortablePlayingInGW.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name            Comfortable Playing In GW
+// @namespace       https://github.com/MyRequiem/comfortablePlayingInGW
+// @description     Веселые плюшки для ganjawars.ru
+// @id              comfortablePlayingInGW@MyRequiem
+// @updateURL       https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/comfortablePlayingInGW.meta.js
+// @downloadURL     https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/_comfortablePlayingInGW.user.js
+// @include         http://www.ganjawars.ru/*
+// @include         http://quest.ganjawars.ru*
+// @grant           none
+// @license         MIT
+// @version         1.0-310715-dev
+// @author          MyRequiem [http://www.ganjawars.ru/info.php?id=2095458]
+// ==/UserScript==
