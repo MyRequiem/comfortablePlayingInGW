46905bf2ccff791aa1c934ef9232a3cea92f2e16
MyRequiem <mailto:mrvladislavovich@gmail.com>
2016-04-08 19:20:45 +0400

    modified generateJSDoc.sh && update jsdoc

--- generateJSDoc.sh
+++ generateJSDoc.sh
@@ -2,16 +2,16 @@
 
 JSDOCPATH=/home/myrequiem/projects/git/jsdoc
 PROJECTPATH=/home/myrequiem/projects/git/comfortablePlayingInGW
-SCRIPTNAME=_comfortablePlayingInGW.user.js
-NEWSCRIPTNAME=$(echo ${SCRIPTNAME} | cut -d _ -f 2)
+SCRIPTNAME=_comfortablePlayingInGW_src.user.js
+NEWSCRIPTNAME=$(echo ${SCRIPTNAME} | cut -d _ -f 2-)
 
 if [ "${PROJECTPATH}" != "$(pwd)" ]; then
     echo "Not a git repository \"comfortablePlayingInGW\""
     exit
 fi
 
-cd ${JSDOCPATH} && \
+cd ${JSDOCPATH}
 cp ${PROJECTPATH}/${SCRIPTNAME} ${NEWSCRIPTNAME}
-./jsdoc.js --verbose --destination ${PROJECTPATH}/jsdoc ${NEWSCRIPTNAME} && \
-    rm ${NEWSCRIPTNAME}
+./jsdoc.js --verbose --destination ${PROJECTPATH}/jsdoc ${NEWSCRIPTNAME}
+rm -f ${NEWSCRIPTNAME}
 
