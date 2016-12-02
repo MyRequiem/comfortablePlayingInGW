1cf6a5825caaaa8beca614fad29559ebc734b6b5
MyRequiem <mailto:mrvladislavovich@gmail.com>
2016-12-02 12:27:59 +0400

    fix changeVersion.sh, generateJSDoc.sh and update jsdoc

--- generateJSDoc.sh
+++ generateJSDoc.sh
@@ -1,17 +1,23 @@
 #! /bin/sh
 
-JSDOCPATH=/home/myrequiem/projects/git/jsdoc
-PROJECTPATH=/home/myrequiem/projects/git/gw/comfortablePlayingInGW
-SCRIPTNAME=_comfortablePlayingInGW.user.js
-NEWSCRIPTNAME=$(echo ${SCRIPTNAME} | cut -d _ -f 2-)
-
-if [ "${PROJECTPATH}" != "$(pwd)" ]; then
-    echo "Not a git repository \"comfortablePlayingInGW\""
-    exit
+# if jsdoc not installed
+if [ "x$(which jsdoc 2>/dev/null)" == "x" ]; then
+    echo "Command 'jsdoc' not found in your PATH."
+    echo "Installing jsdoc:"
+    echo "  $ cd ~/bin"
+    echo "  $ npm install jsdoc"
+    echo "  $ ln -s node_modules/jsdoc/jsdoc.js jsdoc"
+    exit 1
 fi
 
-cd ${JSDOCPATH}
-cp ${PROJECTPATH}/${SCRIPTNAME} ${NEWSCRIPTNAME}
-./jsdoc.js --verbose --destination ${PROJECTPATH}/jsdoc ${NEWSCRIPTNAME}
-rm -f ${NEWSCRIPTNAME}
+PROJECTNAME=comfortablePlayingInGW
+SCRIPTNAME=_${PROJECTNAME}.user.js
+NEWSCRIPTNAME=$(echo ${SCRIPTNAME} | cut -d _ -f 2-)
 
+if [ -d ./.git ] && [ "$(basename "$(pwd)")" == ${PROJECTNAME} ]; then
+    cp ${SCRIPTNAME} "${NEWSCRIPTNAME}"
+    jsdoc --verbose --destination ./jsdoc "${NEWSCRIPTNAME}"
+    rm -f "${NEWSCRIPTNAME}"
+else
+    echo "Not a git repository \"comfortablePlayingInGW\""
+fi
