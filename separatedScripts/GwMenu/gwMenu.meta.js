317f506bf04bb8f590e8d43cf190609e7bba08ed
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-04-11 09:35:01 +0400

    gwMenu.user.js: remove noinspection Annotator

--- separatedScripts/GwMenu/gwMenu.meta.js
+++ separatedScripts/GwMenu/gwMenu.meta.js
@@ -8,6 +8,6 @@
 // @include         http://www.ganjawars.ru*
 // @grant           none
 // @license         MIT
-// @version         2.34-100418
+// @version         2.34-110418
 // @author          MyRequiem [http://www.ganjawars.ru/info.php?id=2095458]
 // ==/UserScript==
