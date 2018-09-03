0ea7f803c298b16ae773421ce9f877fa928f70cd
Vladimir <mrvladislavovich@gmail.com>
2018-09-03 16:08:34 +0400

    fix imgs/ImgPokemonsOnBattle/0_rename_old_names.sh

--- imgs/ImgPokemonsOnBattle/0_rename_old_names.sh
+++ imgs/ImgPokemonsOnBattle/0_rename_old_names.sh
@@ -8,10 +8,15 @@ for NEW_NAME in $(cat ${NEW_NAMES}); do
     if ! ls "./${NEW_NAME}.${EXT}" &>/dev/null; then
         for IMG in $(find . -type f -name "*.${EXT}" | sort); do
             NAME="$(basename "${IMG}" .${EXT})"
-            if ! /bin/grep -q ^"${NAME}"$ ${NEW_NAMES} 2>/dev/null; then
-                echo "move ${NAME}.${EXT} --> ${NEW_NAME}.${EXT}"
-                mv "${NAME}.${EXT}"  "${NEW_NAME}.${EXT}"
-                break
+            if [[ "x${NAME}" != "xomon" && \
+                    "x${NAME}" != "xscreen1" && \
+                    "x${NAME}" != "xscreen2" && \
+                    "x${NAME}" != "xscreen3" ]]; then
+                if ! /bin/grep -q ^"${NAME}"$ ${NEW_NAMES} 2>/dev/null; then
+                    echo "move ${NAME}.${EXT} --> ${NEW_NAME}.${EXT}"
+                    mv "${NAME}.${EXT}"  "${NEW_NAME}.${EXT}"
+                    break
+                fi
             fi
         done
     fi
