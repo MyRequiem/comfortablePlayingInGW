f955d6c881ff2d82fe0f8768fc590a1d11a3ce20
Vladimir <mrvladislavovich@gmail.com>
2018-09-14 12:06:53 +0400

    version updated from 1.105-240818 to 1.106-140918

--- _comfortablePlayingInGW.meta.js
+++ _comfortablePlayingInGW.meta.js
@@ -12,6 +12,6 @@
 // @include         *ganjafile.ru*
 // @grant           none
 // @license         MIT
-// @version         1.105-240818
+// @version         1.106-140918
 // @author          MyRequiem [http://www.ganjawars.ru/info.php?id=2095458]
 // ==/UserScript==
