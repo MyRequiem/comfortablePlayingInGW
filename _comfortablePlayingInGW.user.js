5df4ad42d419abafdc7c7cda9bcbeab84e18d33d
Vladimir <mrvladislavovich@gmail.com>
2018-10-15 16:13:09 +0400

    version updated to 1.114-151018

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -12,7 +12,7 @@
 // @include         http://www.ganjafoto.ru*
 // @grant           none
 // @license         MIT
-// @version         1.114-131018
+// @version         1.114-151018
 // @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
 
@@ -81,7 +81,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.114-131018';
+        this.version = '1.114-151018';
         /**
          * @property stString
          * @type {String}
