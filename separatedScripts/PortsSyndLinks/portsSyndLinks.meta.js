93a59614706d52c5f792ef7ddbed67a66649b475
Vladimir <mrvladislavovich@gmail.com>
2018-11-04 10:36:33 +0400

    service files updated

---
+++ separatedScripts/PortsSyndLinks/portsSyndLinks.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name            PortsSyndLinks
+// @namespace       https://github.com/MyRequiem/comfortablePlayingInGW
+// @description     На страницах списков ближайших/прошедших боев за порты добавляет знаки синдикатов, являющиеся ссылками на их онлайн.
+// @id              comfortablePlayingInGW@MyRequiem
+// @updateURL       https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/separatedScripts/PortsSyndLinks/portsSyndLinks.meta.js
+// @downloadURL     https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/separatedScripts/PortsSyndLinks/portsSyndLinks.user.js
+// @include         http://www.gwars.ru/object.php?id=*
+// @grant           none
+// @license         MIT
+// @version         1.01-041118
+// @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
+// ==/UserScript==
