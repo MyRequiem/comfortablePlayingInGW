54e86eafa0004edf157341c3da6bd419033b7409
Vladimir <mrvladislavovich@gmail.com>
2018-11-19 15:12:02 +0400

    Optimize RangeWeapon module code

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -8942,16 +8942,10 @@
         this.init = function () {
             if (this.equipment &&
                     (/(Левая|Правая) рука/.test(this.equipment.innerHTML))) {
-                var itemLink = 'a[href*="/item.php?item_id="]';
-                // новое оформление страницы информации о персонаже
-                this.weapon = this.equipment.
-                        querySelectorAll('td[valign="top"]>' + itemLink);
-                // примитивное оформление страницы информации о персонаже
-                if (!this.weapon.length) {
-                    this.weapon = this.equipment.querySelectorAll(itemLink);
-                }
+                var css = 'td[valign="top"]>a[href*="/item.php?item_id="]',
+                    txt = this.equipment.innerHTML;
 
-                var txt = this.equipment.innerHTML;
+                this.weapon = this.equipment.querySelectorAll(css);
                 if (/Левая/.test(txt) && (/Правая/.test(txt))) {
                     this.weapon = [this.weapon[0].href, this.weapon[1].href];
                 } else {
