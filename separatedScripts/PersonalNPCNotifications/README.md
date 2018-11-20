1dcf263de507859b0a2f1506ff3fd551fbb371a3
Vladimir <mrvladislavovich@gmail.com>
2018-11-20 17:03:51 +0400

    service files updated

---
+++ separatedScripts/PersonalNPCNotifications/README.md
@@ -0,0 +1,10 @@
+Если личный NPC "Ожидает распоряжений", то на главной странице персонажа ссылка на NPC будет "пульсировать".
+<br>
+<br>
+Версия: 1.00-201118
+<br>
+[[Установить]](https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/separatedScripts/PersonalNPCNotifications/personalNPCNotifications.user.js) [[Скачать zip-архив]](https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/separatedScripts/PersonalNPCNotifications/personalNPCNotifications.user.js.zip)
+<br>
+<br>
+![PersonalNPCNotifications](https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/imgs/PersonalNPCNotifications/screen.gif)
+<br>
---
+++ separatedScripts/PersonalNPCNotifications/personalNPCNotifications.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name            PersonalNPCNotifications
+// @namespace       https://github.com/MyRequiem/comfortablePlayingInGW
+// @description     Если личный NPC "Ожидает распоряжений", то на главной странице персонажа ссылка на NPC будет "пульсировать".
+// @id              comfortablePlayingInGW@MyRequiem
+// @updateURL       https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/separatedScripts/PersonalNPCNotifications/personalNPCNotifications.meta.js
+// @downloadURL     https://raw.githubusercontent.com/MyRequiem/comfortablePlayingInGW/master/separatedScripts/PersonalNPCNotifications/personalNPCNotifications.user.js
+// @include         http://www.gwars.ru/me.php*
+// @grant           none
+// @license         MIT
+// @version         1.00-201118
+// @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
+// ==/UserScript==
