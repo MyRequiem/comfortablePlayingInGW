283aadaf13b7c5646f9d8dbd4fc7fa330c7a0758
Vladimir <mrvladislavovich@gmail.com>
2019-02-20 17:53:33 +0400

    version updated -->  1.127-200219

--- _comfortablePlayingInGW.meta.js
+++ _comfortablePlayingInGW.meta.js
@@ -12,6 +12,6 @@
 // @include         http://www.ganjafoto.ru*
 // @grant           none
 // @license         MIT
-// @version         1.126-190219
+// @version         1.127-200219
 // @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
