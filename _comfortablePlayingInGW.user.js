523c4e4b9a1fafa14077663a33c17286cc5a5b9a
Vladimir <mrvladislavovich@gmail.com>
2019-09-07 10:09:09 +0400

    DoFilter module fixed

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -8063,7 +8063,8 @@
          * @method init {{{2
          */
         this.init = function () {
-            var target = general.doc.querySelector('table+div+br+center');
+            var target = general.doc.querySelector('table+div+br+center') ||
+                            general.doc.querySelector('table+br+center');
 
             if (!this.selects.length || !this.selects[0].options || !target) {
                 return;
