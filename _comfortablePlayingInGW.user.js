21a1676a6a95a7f7961162fdb03cb97009aebc39
Vladimir <mrvladislavovich@gmail.com>
2019-10-06 16:23:19 +0400

    version updated --> 1.143-061019

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -12,7 +12,7 @@
 // @include         http://www.ganjafoto.ru*
 // @grant           none
 // @license         MIT
-// @version         1.142-210919
+// @version         1.143-061019
 // @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
 
@@ -83,7 +83,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.142-210919';
+        this.version = '1.143-061019';
         /**
          * @property stString {{{2
          * @type {String}
