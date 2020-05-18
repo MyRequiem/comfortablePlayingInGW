f7debdc6815141919d3d2224148e630a64502acd
Vladimir <mrvladislavovich@gmail.com>
2020-05-18 17:38:37 +0400

    Service files updated

--- separatedScripts/SoundSyndBattle/soundSyndBattle.meta.js
+++ separatedScripts/SoundSyndBattle/soundSyndBattle.meta.js
@@ -8,6 +8,6 @@
 // @include         http://www.gwars.ru/*
 // @grant           none
 // @license         MIT
-// @version         2.19-190819
+// @version         2.20-180520
 // @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
