1c3e8b24ac2147658c37c12f30726be46b21fb6a
Vladimir <mrvladislavovich@gmail.com>
2020-05-20 20:28:06 +0400

    Version updated --> 1.153-200520

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -12,7 +12,7 @@
 // @include         http://www.ganjafoto.ru*
 // @grant           none
 // @license         MIT
-// @version         1.152-180520
+// @version         1.153-200520
 // @author          MyRequiem [http://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
 
@@ -83,7 +83,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.152-180520';
+        this.version = '1.153-200520';
         /**
          * @property stString {{{2
          * @type {String}
