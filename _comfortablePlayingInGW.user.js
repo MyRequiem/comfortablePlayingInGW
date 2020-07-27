8945eff85a6c54acacd79e8416bfb54e1571c681
Vladimir <mrvladislavovich@gmail.com>
2020-07-27 16:56:04 +0400

    DoFilter module removed

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -112,7 +112,7 @@
                         [18] - BonusInfo
                         [19] - BuyHightech
                         [20] - NewsAndInvit
-                        [21] - DoFilter
+                        [21] - DoFilter (удален)
                         [22] - FilterResOnStat
                         [23] - FilterWarlistOne2One
                         [24] - FixSkills
@@ -1274,11 +1274,6 @@
                     'для каждого предмета, при нажатии на которые, выводится ' +
                     'форма подачи объявления на ДО для данного предмета.' +
                     this.getGitHubLink('buyHightech'), '19'],
-                ['Быстрый поиск на ДО', 'Быстрый поиск предметов на ' +
-                    '<a target="_blank" href="http://www.gwars.ru' +
-                    '/market.php">доске объявлений</a>. Поиск осуществляется ' +
-                    'по мере ввода названия предмета.' +
-                    this.getGitHubLink('doFilter'), '21'],
                 ['Фильтр ресурсов на странице экономической статистики',
                     'Фильтр ресурсов на <a target="_blank" ' +
                     'href="http://www.gwars.ru/stats.php">странице ' +
@@ -8014,141 +8009,6 @@
     }; // 1}}}
 
     /**
-     * @class DoFilter {{{1
-     * @constructor
-     */
-    var DoFilter = function () {
-        /**
-         * @property selects
-         * @type {NodeList}
-         */
-        this.selects = general.doc.querySelectorAll('select[name="item_id"]');
-
-        /**
-         * @method setHrefItem {{{2
-         * @param   {HTMLSelectElement}     sel
-         */
-        this.setHrefItem = function (sel) {
-            var a = sel.nextElementSibling,
-                itemId = sel.value;
-
-            if (itemId !== '#') {
-                a.href = 'http://www.gwars.ru/item.php?item_id=' + itemId;
-                a.setAttribute('target', '_blank');
-            } else {
-                a.href = itemId;
-                a.removeAttribute('target');
-            }
-        }; // 2}}}
-
-        /**
-         * @method selectChange {{{2
-         * @param   {HTMLSelectElement}     sel
-         */
-        this.selectChange = function (sel) {
-            var _this = this;
-
-            return function () {
-                _this.setHrefItem(sel);
-            };
-        }; // 2}}}
-
-        /**
-         * @method findItem {{{2
-         * @param   {Object}  inp
-         */
-        this.findItem = function (inp) {
-            var i, j;
-            for (i = 0; i < this.selects.length; i++) {
-                // если текстовое поле пустое(стерто BackSpase'ом),
-                // то вернем списки в начальное состояние
-                if (!inp.value) {
-                    this.selects[i].options[1].selected = true;
-                    this.setHrefItem(this.selects[i]);
-                    continue;
-                }
-
-                // выбираем самый первый пустой option
-                // noinspection JSUndefinedPropertyAssignment
-                this.selects[i].value = '#';
-                // прокручиваем весь список и ищем совпадения
-                for (j = 0; j < this.selects[i].options.length; j++) {
-                    if (this.selects[i].options[j].innerHTML.toLowerCase().
-                            indexOf(inp.value.toLowerCase()) !== -1) {
-                        this.selects[i].options[j].selected = true;
-                    }
-                }
-
-                this.setHrefItem(this.selects[i]);
-            }
-        }; // 2}}}
-
-        /**
-         * @method init {{{2
-         */
-        this.init = function () {
-            var target = general.doc.querySelector('table+div+div+br+center') ||
-                            general.doc.querySelector('table+br+center');
-
-            if (!this.selects.length || !this.selects[0].options || !target) {
-                return;
-            }
-
-            var opt, a, i;
-            for (i = 0; i < this.selects.length; i++) {
-                // одинаковая длина у всех списков
-                this.selects[i].setAttribute('style', 'width: 190px;');
-
-                // добавим пустой елемент в select
-                opt = general.doc.createElement('option');
-                opt.innerHTML = '&nbsp';
-                opt.setAttribute('value', '#');
-                this.selects[i].insertBefore(opt,
-                        this.selects[i].firstElementChild);
-                // выделен первый option (не пустой)
-                this.selects[i].options[1].selected = true;
-
-                // добавим ссылки на предметы после селектов
-                a = general.doc.createElement('a');
-                a.innerHTML = '[?]';
-                a.setAttribute('title', 'Страница описания предмета');
-                a.setAttribute('style', 'margin-left: 2px; color: #808080; ' +
-                        'text-decoration: none;');
-                // noinspection JSUnresolvedVariable
-                if (general.root.crng) {
-                    this.selects[i].parentNode.appendChild(a);
-                }
-
-                // обработчик 'onchange' при изменении списка выбора
-                this.selects[i].addEventListener('change',
-                        this.selectChange(this.selects[i]), false);
-
-                // устанавливаем атрибут href ссылки
-                this.setHrefItem(this.selects[i]);
-            }
-
-            // вставляем текстовое поле ввода
-            // noinspection JSUnresolvedVariable
-            if (general.root.crng) {
-                var divSearch = general.doc.createElement('div');
-                divSearch.innerHTML = '<span style="color: #008000; ' +
-                    'font-weight: bold;">Быстрый поиск:</span> <input ' +
-                    'id="txtFilter" size="40" style="margin-bottom: ' +
-                    '10px;">';
-                target.insertBefore(divSearch, target.firstChild);
-            }
-
-            var textField = general.doc.querySelector('#txtFilter'),
-                _this = this;
-
-            textField.addEventListener('input', function () {
-                _this.findItem(textField);
-            }, false);
-            textField.focus();
-        }; // 2}}}
-    }; // 1}}}
-
-    /**
      * @class FilterResOnStat {{{1
      * @constructor
      */
@@ -14387,16 +14247,6 @@
                         } catch (e) {
                             general.cons.log(e);
                         }
-                    }
-                }
-            }
-
-            if (/\/market(-p)?.php/.test(general.loc)) {
-                if (initScript[21]) {
-                    try {
-                        new DoFilter().init();
-                    } catch (e) {
-                        general.cons.log(e);
                     }
                 }
             }
