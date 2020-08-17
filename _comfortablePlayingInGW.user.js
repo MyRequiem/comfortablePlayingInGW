b8e2c414afb689ef0c28bae44bfe573ddfe46bb0
Vladimir <mrvladislavovich@gmail.com>
2020-08-17 11:48:31 +0400

    1.160-170820

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -10,7 +10,7 @@
 // @include         http*://*ganjafile.ru*
 // @grant           none
 // @license         MIT
-// @version         1.159-300720
+// @version         1.160-170820
 // @author          MyRequiem [https://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
 
@@ -81,7 +81,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.159-300720';
+        this.version = '1.160-170820';
         /**
          * @property stString {{{2
          * @type {String}
