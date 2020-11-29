34f8dc7e74c7b3689116a0d5061f1a8d004a9615
Vladimir <mrvladislavovich@gmail.com>
2020-11-29 19:29:44 +0400

    Version updated --> 1.165-291120

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -10,7 +10,7 @@
 // @include         http*://*ganjafile.ru*
 // @grant           none
 // @license         MIT
-// @version         1.164-300920
+// @version         1.165-291120
 // @author          MyRequiem [https://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
 
@@ -81,7 +81,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.164-300920';
+        this.version = '1.165-291120';
         /**
          * @property stString {{{2
          * @type {String}
