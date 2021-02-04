5d7a5458050860beecb5a45e429cfca438d47074
Vladimir <mrvladislavovich@gmail.com>
2021-02-04 11:45:12 +0400

    Version updated --> 1.167-040221

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -10,7 +10,7 @@
 // @include         http*://*ganjafile.ru*
 // @grant           none
 // @license         MIT
-// @version         1.166-120121
+// @version         1.167-040221
 // @author          MyRequiem [https://www.gwars.ru/info.php?id=2095458]
 // ==/UserScript==
 
@@ -81,7 +81,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.166-120121';
+        this.version = '1.167-040221';
         /**
          * @property stString {{{2
          * @type {String}
