b38a29e4f6f7c2a39c7038981ce498c43a24f4ac
Vladimir <mrvladislavovich@gmail.com>
2024-04-20 18:46:06 +0400

    Version updated --> 1.176-200424

--- _comfortablePlayingInGW.user.js
+++ _comfortablePlayingInGW.user.js
@@ -10,7 +10,7 @@
 // @include         http*://*ganjafile*
 // @grant           none
 // @license         MIT
-// @version         1.175-170522
+// @version         1.176-200424
 // @author          MyRequiem [https://www.gwars.io/info.php?id=2095458]
 // ==/UserScript==
 
@@ -81,7 +81,7 @@
          * @property version
          * @type {String}
          */
-        this.version = '1.175-170522';
+        this.version = '1.176-200424';
         /**
          * @property stString {{{2
          * @type {String}
